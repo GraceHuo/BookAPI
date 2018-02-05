29ceb8b554faf965628e6a8e305606d43ef1ba1c
Grace Huo <Grace.Huo@intersystems.com>
2018-02-05 13:09:42 -0500

    post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express  = require( 'express' );
 var mongoose = require( 'mongoose' );
+var bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
 
@@ -9,43 +10,16 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 app.get( '/', function( req, res ) {
   res.send( 'Welcome' );
 } );
 
-var bookRouter = express.Router();
+var bookRouter = require('./Routes/bookRoutes')(Book);
 
-bookRouter.route( '/Books' )
-  .get( function( req, res ) {
-    var query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-
-    Book.find(query, function( err, books ) {
-      if(err) {
-        res.status(500).send(err);
-      }
-      else {
-        res.json(books);
-      }
-    });
-  } );
-
-bookRouter.route( '/Books/:bookId' )
-  .get( function( req, res ) {
-
-    Book.findById(req.params.bookId, function( err, book ) {
-      if(err) {
-        res.status(500).send(err);
-      }
-      else {
-        res.json(book);
-      }
-    });
-  } );
-
-app.use( '/api', bookRouter );
+app.use( '/api/books', bookRouter );
 
 app.listen( port, function() {
   console.log( 'Running on PORT: ' + port );
