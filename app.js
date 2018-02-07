684641cf9e9731e1b7af08f6f87e15d4afb206bb
Grace Huo <Grace.Huo@intersystems.com>
2018-02-07 10:40:39 -0500

    tests

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-var express  = require( 'express' );
-var mongoose = require( 'mongoose' );
-var bodyParser = require('body-parser');
+var express    = require( 'express' );
+var mongoose   = require( 'mongoose' );
+var bodyParser = require( 'body-parser' );
 
-var db = mongoose.connect('mongodb://localhost/bookAPI');
+var db;
+if ( process.env.ENV == 'Test' ) {
+  db = mongoose.connect( 'mongodb://localhost/bookAPI_test' );
+}
+else {
+  db = mongoose.connect( 'mongodb://localhost/bookAPI' );
+}
 
-var Book = require('./models/bookModel');
+var Book = require( './models/bookModel' );
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.json() );
 
 app.get( '/', function( req, res ) {
   res.send( 'Welcome' );
 } );
 
-var bookRouter = require('./Routes/bookRoutes')(Book);
+var bookRouter = require( './routes/bookRoutes' )( Book );
 
 app.use( '/api/books', bookRouter );
 
 app.listen( port, function() {
   console.log( 'Running on PORT: ' + port );
 } );
+
+module.exports = app;
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
-var gulp    = require( 'gulp' ),
-    nodemon = require( 'gulp-nodemon' );
+var gulp      = require( 'gulp' ),
+    nodemon   = require( 'gulp-nodemon' ),
+    gulpMocha = require( 'gulp-mocha' ),
+env = require('gulp-env'),
+supertest = require('supertest');
 
 gulp.task( 'default', function() {
   nodemon( {
     script: 'app.js',
-    ext : 'js',
-    env : {
-      PORT: 8000
-    },
+    ext   : 'js',
+    env   : { PORT: 8000 },
     ignore: ['./node_modules/**']
   } )
-    .on('restart', function(  ) {
+    .on( 'restart', function() {
       console.log( 'Restarted!' );
-    })
+    } )
+} );
+
+gulp.task( 'test', function() {
+  env({vars: {ENV:'Test'}});
+  gulp.src( 'tests/*.js', { read: false } )
+    .pipe( gulpMocha( { reporter: 'nyan' } ) );
 } );
